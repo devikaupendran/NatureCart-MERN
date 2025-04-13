d38abc4651987e850b37df9c087e75b61d3ba630
devikaupendran <devikaupendranr@gmail.com>
2025-04-13 13:05:47 +0530

    added logout function

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -8,6 +8,12 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
     const { user, setUser, navigate, setShowUserLogin } = useAppContext();
 
+    // Logout handler
+    const logout = async () => {
+        setUser(null);
+        navigate('/');
+    }
+
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
