c6454cfa60e5a8b7229e5e3d9fe8b09ac37ce57f
devikaupendran <devikaupendranr@gmail.com>
2025-04-13 20:34:43 +0530

    created bottom banner

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../Components/Banner'
 import Categories from '../Components/Categories'
 import BestSellers from '../Components/BestSellers'
+import BottomBanner from '../Components/BottomBanner'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <Categories />
             <BestSellers />
+            <BottomBanner />
         </div>
     )
 }
