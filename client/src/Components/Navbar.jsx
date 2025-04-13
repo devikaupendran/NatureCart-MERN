e7894de2ac08a9050af1e527d6597cc75571d9b1
devikaupendran <devikaupendranr@gmail.com>
2025-04-13 13:00:21 +0530

    added states in appcontext and use it in navbar for user state based rendering

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { assets } from '../assets/assets'
 import { NavLink } from 'react-router-dom'
+import { useAppContext } from '../Contexts/AppContext';
 
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const { user, setUser, navigate, setShowUserLogin } = useAppContext();
 
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
@@ -32,9 +34,22 @@ const Navbar = () => {
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-green-700 w-[18px] h-[18px] rounded-full">0</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-green-700 hover:bg-green-600 transition text-white rounded-full">
-                    Login
-                </button>
+                {/* if there is no user then show login button otherwise show profile image with a dropdown (my orders, logout)  */}
+                {
+                    !user ? (
+                        <button className="cursor-pointer px-8 py-2 bg-green-700 hover:bg-green-600 transition text-white rounded-full" onClick={() => setShowUserLogin(true)}>
+                            Login
+                        </button>
+                    ) : (
+                        <div className='relative group'>
+                            <img src={assets.profile_icon} alt="profile icon" className='w-10' />
+                            <ul className='hidden w-max group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-3  text-sm rounded-md'>
+                                <li className='p-2 px-6 hover:bg-green-100 cursor-pointer' onClick={() => navigate('/my-orders')}>My orders</li>
+                                <li className='p-2 px-6 hover:bg-green-100 cursor-pointer' onClick={logout}>Logout</li>
+                            </ul>
+                        </div>
+                    )
+                }
 
             </div>
 
@@ -49,14 +64,26 @@ const Navbar = () => {
                     <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] z-2 left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                         <NavLink to={'/'} onClick={() => setOpen(false)} className="block py-3">Home</NavLink>
                         <NavLink to={'/products'} onClick={() => setOpen(false)} className="block py-3">Our products</NavLink>
-                        <NavLink to={'/'} onClick={() => setOpen(false)} className="block py-3">My orders</NavLink>
+
+                        {/* if there is user then only show my orders option in menubar */}
+                        {user && <NavLink to={'/'} onClick={() => setOpen(false)} className="block py-3">My orders</NavLink>}
+
                         <NavLink to={'/'} onClick={() => setOpen(false)} className="block py-3">Contact us</NavLink>
 
-                        <button onClick={() => { setOpen(false); }}
-                            className="cursor-pointer px-6 py-2 mt-2 bg-green-700 hover:bg-green-600 transition text-white rounded-full text-sm">
-                            Login
-                        </button>
-
+                        {
+                            !user ? (
+                                <button onClick={() => { setOpen(false); setShowUserLogin(true) }}
+                                    className="cursor-pointer px-6 py-2 mt-2 bg-green-700 hover:bg-green-600 transition text-white rounded-full text-sm">
+                                    Login
+                                </button>
+                            )
+                                : (
+                                    <button onClick={logout}
+                                        className="cursor-pointer px-6 py-2 mt-2 bg-green-700 hover:bg-green-600 transition text-white rounded-full text-sm">
+                                        Logout
+                                    </button>
+                                )
+                        }
                     </div>)
             }
         </nav>
--- client/src/Contexts/AppContext.jsx
+++ client/src/Contexts/AppContext.jsx
@@ -1,9 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-    const value = {}
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+
+    const value = {
+        navigate,
+        user, setUser,
+        isSeller, setIsSeller,
+        showUserLogin, setShowUserLogin,
+    }
     return (
         <AppContext.Provider value={value}>
             {children}
