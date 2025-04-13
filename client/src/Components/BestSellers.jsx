3b0c4bbf695ba6ffca2cd9530cf289bfdaa0f8ff
devikaupendran <devikaupendranr@gmail.com>
2025-04-13 14:40:36 +0530

    created state and function for displaying products use it in bestseller for filtering and pass to productCard using props

--- client/src/Components/BestSellers.jsx
+++ client/src/Components/BestSellers.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import { useAppContext } from '../context/AppContext'
 
-const BestSellers = () => {
+const BestSeller = () => {
+    
+    const { products } = useAppContext();
+
     return (
         <div className='mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>Best sellers</p>
-            <ProductCard />
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+
+                {
+                    //filter the products based on stock and displayed 5 products and pass that data to product card for displaying
+                    products.filter((product) => product.inStock).slice(0, 5)
+                        .map((product, index) => {
+                            return (
+                                <ProductCard key={index} product={product} />
+                            )
+                        })
+                }
+            </div>
         </div>
     )
 }
 
-export default BestSellers
+export default BestSeller
--- client/src/Contexts/AppContext.jsx
+++ client/src/Contexts/AppContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 export const AppContext = createContext();
 
@@ -10,12 +11,23 @@ export const AppContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
 
+    //fetch all products from asset file when page load 
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    }
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    
     const value = {
         navigate,
         user, setUser,
         isSeller, setIsSeller,
         showUserLogin, setShowUserLogin,
+        products, setProducts
     }
     return (
         <AppContext.Provider value={value}>
