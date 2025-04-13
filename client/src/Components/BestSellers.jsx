c75519254852e526926c49b8fe8eb60a7f47881f
devikaupendran <devikaupendranr@gmail.com>
2025-04-13 13:55:24 +0530

    created a dummy product card

--- client/src/Components/BestSellers.jsx
+++ client/src/Components/BestSellers.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
 const BestSellers = () => {
     return (
         <div className='mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>Best sellers</p>
+            <ProductCard />
         </div>
     )
 }
