c3d46ec0aca19e88376aa40769da99954cf4b76b
devikaupendran <devikaupendranr@gmail.com>
2025-04-15 11:06:26 +0530

    created product details with products data. and created function for load  related products and thumbnail

--- client/src/Pages/ProductDetails.jsx
+++ client/src/Pages/ProductDetails.jsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets';
+import { useAppContext } from '../Contexts/AppContext';
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
-    const product = {
-        name: "Strawberry",
-        category: "fruit",
-        price: 100,
-        offerPrice: 80,
-        images: [
-            "https://www.freeiconspng.com/uploads/strawberry-png-30.png",
-            "https://img.freepik.com/free-psd/fresh-strawberries-splash-juice-transparent-background_84443-25724.jpg?semt=ais_hybrid&w=740",
-            "https://w7.pngwing.com/pngs/983/762/png-transparent-strawberry-fruit-strawberry-strawberry-clipart-thumbnail.png",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe3JEQ12QZQRKVK_bW33TUlQDv3RgR0T7d3g&s"
-        ],
-        description: [
-            "Naturally sweet and juicy flavor",
-            "Perfect for snacking, baking, or blending",
-            "Rich in vitamins, antioxidants, and fiber"
-        ]
-    };
-    const [thumbnail, setThumbnail] = useState(product.images[0]);
+
+    const { products, navigate, addToCart, currencySymbol } = useAppContext();
+    const { id } = useParams();
+
+    const product = products.find((item) => item._id === id);
+
+    const [relatedProducts, setRelatedProducts] = useState([]);
+    const [thumbnail, setThumbnail] = useState(null);
+
+    //Filters out products in the same category.Takes only the first 5 to show as related products.Stores them in state for display.
+    useEffect(() => {
+        if (products.length > 0) {
+            let productsCopy = [...products];
+            productsCopy = productsCopy.filter((item) => product.category === item.category);
+            setRelatedProducts(productsCopy.slice(0, 5))
+        }
+    }, [products]);
+
+    //When the product is set (or changes), the first image is selected as the default thumbnail to be displayed.
+    useEffect(() => {
+        setThumbnail(product?.image[0] ? product.image[0] : null)
+    }, [product])
 
     return product && (
-        <div className="max-w-6xl w-full px-6 mt-24">
+        <div className="mt-12">
 
             {/* Breadcrumbs */}
             <p>
@@ -32,10 +38,11 @@ const ProductDetails = () => {
                 <span className="text-green-700"> {product.name}</span>
             </p>
 
+            {/* images section  */}
             <div className="flex flex-col md:flex-row gap-16 mt-4">
                 <div className="flex gap-3">
                     <div className="flex flex-col gap-3">
-                        {product.images.map((image, index) => (
+                        {product.image.map((image, index) => (
                             <div key={index} onClick={() => setThumbnail(image)} className="border max-w-24 border-gray-500/30 rounded overflow-hidden cursor-pointer" >
                                 <img src={image} alt={`Thumbnail ${index + 1}`} />
                             </div>
@@ -48,6 +55,7 @@ const ProductDetails = () => {
                 </div>
 
                 <div className="text-sm w-full md:w-1/2">
+                    {/* ----- name ----- */}
                     <h1 className="text-3xl font-medium">{product.name}</h1>
 
                     <div className="flex items-center gap-0.5 mt-1">
@@ -62,19 +70,21 @@ const ProductDetails = () => {
                         </div>
                     </div>
 
+                    {/* price and offer price section */}
                     <div className="mt-6">
                         <p className="text-gray-500/70 line-through">MRP: ${product.price}</p>
                         <p className="text-2xl font-medium">MRP: ${product.offerPrice}</p>
                         <span className="text-gray-500/70">(inclusive of all taxes)</span>
                     </div>
 
+                    {/* About product  */}
                     <p className="text-base font-medium mt-6">About Product</p>
                     <ul className="list-disc ml-4 text-gray-500/70">
                         {product.description.map((desc, index) => (
                             <li key={index}>{desc}</li>
                         ))}
                     </ul>
-
+                    {/* Add to cart and Buy now nutton */}
                     <div className="flex items-center mt-10 gap-4 text-base">
                         <button className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition" >
                             Add to Cart
@@ -86,6 +96,7 @@ const ProductDetails = () => {
                 </div>
             </div>
         </div>
+
     )
 }
 
